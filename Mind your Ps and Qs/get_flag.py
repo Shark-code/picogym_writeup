17325dbb131dbe1f684f06f6a40ddeb36d2394da
Shark-code <fuhanpan.zzz@gmail.com>
2021-07-04 23:25:40 +0800

    add some comments

--- Mind your Ps and Qs/get_flag.py
+++ Mind your Ps and Qs/get_flag.py
@@ -1,13 +1,20 @@
-
+# values
 c = 843044897663847841476319711639772861390329326681532977209935413827620909782846667
 n = 1422450808944701344261903748621562998784243662042303391362692043823716783771691667
 e = 65537
 
+# n = p * q
 p = 2159947535959146091116171018558446546179
 q = 658558036833541874645521278345168572231473
 
+# Euler function
 fn = (p - 1) * (q - 1)
 
+
+# calculate private key
+#
+#    e * d mod fn = 1
+# => d = (fn * k + 1) / e
 k = 0
 d = 0
 while True:
@@ -17,6 +24,8 @@ while True:
 		break
 	k += 1
 
+# a function to calculate mod
+# => result = a^b mod c
 def big_mod(a, b, c):
 	if b == 0:
 		return 1 % c
@@ -28,7 +37,9 @@ def big_mod(a, b, c):
 	if b % 2 == 1:
 		return (big_mod(a, b - 1, c) * (a % c)) % c
 
+# decrypt message
 m = big_mod(c, d, n)
 
+# transform to valid string
 print(bytearray.fromhex(hex(m).replace("0x", "")).decode())
 
